78f3fc668a3a93f8bfab92f023e73185f40f515e
John Montgomery <johnbmontgomery@gmail.com>
2014-06-15 23:30:32 -0400

    Add very basic setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+# Installation script for wmata package
+from distutils.core import setup
+setup(
+    name='wmata',
+    packages=['wmata'],
+    version='0.1',
+    description='API for accessing WMATA information.'.
+    author='John Montgomery',
+    author_email='johnbmontgomery@gmail.com',
+    keywords=['wmata', 'metro', 'washington', 'dc', 'transit'],
+    classifiers=['TODO: I don\'t know what this is, so I\'ll fill it in later'],
+    long_description="""
+Fill this out later too
+"""
+)
+
