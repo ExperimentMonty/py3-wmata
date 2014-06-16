65ce5829633197783a588ce342117b32d242747d
John Montgomery <johnbmontgomery@gmail.com>
2014-06-16 03:47:43 -0400

    Moves default value into __init__ default values.
    Don't know why I didn't do this in the first place, had some silly idea that it
    looked like it was a long string to have in a function header/people might want
    to see a value like this listed prominently in the code layout.
    
    Thought: change api_key to _api_key? Really shouldn't get used outside this
    object.

--- wmata.py
+++ wmata.py
@@ -7,10 +7,9 @@ class WmataError(Exception):
 
 class Wmata(object):
 
-    base_url = 'http://api.wmata.com/%(svc)s.svc/json/%(endpoint)s'
-    # By default, we'll use the WMATA demonstration key
-    api_key = 'kfgpmgvfgacx98de9q3xazww' 
+    api_url = 'http://api.wmata.com/%(svc)s.svc/json/%(endpoint)s'
 
-    def __init__(self, api_key=None):
+    # By default, we'll use the WMATA demonstration key
+    def __init__(self, api_key='kfgpmgvfgacx98de9q3xazww'):
         if api_key is not None:
             self.api_key = api_key
