27f91b2ad3d3145c8e825b409d804960ebcb8a94
John Montgomery <johnbmontgomery@gmail.com>
2014-09-18 08:04:44 -0400

    Stubs out functions.

--- wmata.py
+++ wmata.py
@@ -12,3 +12,52 @@ class Wmata(object):
     # By default, we'll use the WMATA demonstration key
     def __init__(self, api_key='kfgpmgvfgacx98de9q3xazww'):
         self.api_key = api_key
+
+    def _get(self, function, arguments):
+        pass
+
+    def rail_lines(self):
+        pass
+
+    def rail_stations(self):
+        pass
+
+    def rail_station_info(self):
+        pass
+
+    def rail_station_entrances(self):
+        pass
+
+    def rail_path(self):
+        pass
+
+    def rail_predictions(self):
+        pass
+
+    def rail_incidents(self):
+        pass
+
+    def bus_routes(self):
+        pass
+
+    def bus_route_schedue(self):
+        pass
+
+    def bus_route_details(self):
+        pass
+
+    def bus_positions(self):
+        pass
+
+    def bus_stops(self):
+        pass
+
+    def bus_stop_schedule(self):
+        pass
+
+    def bus_prediction(self):
+        pass
+
+    def elevator_incidents(self, station_code='All'):
+        pass
+
